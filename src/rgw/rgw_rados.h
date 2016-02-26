d030674c9d1d6c685dc9bc4abbdc2a8de8cc431f
Ved-vampir <akiselyova@mirantis.com>
2016-02-26 17:11:30 +0300

    rgw: add decompression & cmpressor attrs save Signed-off-by: Alyona Kiseleva <akiselyova@mirantis.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -3248,6 +3248,7 @@ public:
   CephContext *ctx();
 
   bool is_canceled() { return canceled; }
+  bool is_compressed() { return compressed; }
 }; /* RGWPutObjProcessor */
 
 struct put_obj_aio_info {
